4676d9fe54e7091a6427e8c8f474249c49d05aa1
Patrick Boucly <patrick.boucly@insee.fr>
2021-12-01 07:50:26 +0100

    a2021d1 cleaner

--- src/main/java/aocmaven/a2021/A2021Day1.java
+++ src/main/java/aocmaven/a2021/A2021Day1.java
@@ -1,8 +1,5 @@
 package aocmaven.a2021;
 
-import static org.apache.commons.io.FileUtils.readFileToString;
-
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +7,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class A2021Day1 {
 
@@ -20,32 +18,28 @@ public class A2021Day1 {
 
 	private static void s1() {
 		String input=read();
-		List<String> inputL=Arrays.asList(input.split("\n"));
+		List<Integer> inputL=Arrays.asList(input.split("\n")).stream().map(Integer::parseInt).collect(Collectors.toList());
+		System.out.println("s1 : "+getNbIncrease(inputL));
+	}
+
+	private static int getNbIncrease(List<Integer> inputL) {
 		int res=0;
 		for(int i=1;i<inputL.size();i++) {
-			if(Integer.parseInt(inputL.get(i-1))<Integer.parseInt(inputL.get(i))){
+			if(inputL.get(i-1)<inputL.get(i)){
 				res++;
 			}
 		}
-		System.out.println("s1 : "+res);
+		return res;
 	}
 	
 	private static void s2() {
 		String input=read();
-		List<String> inputL=Arrays.asList(input.split("\n"));
-		int res=0;
+		List<Integer> inputL=Arrays.asList(input.split("\n")).stream().map(Integer::parseInt).collect(Collectors.toList());
 		List<Integer> sommeDe3= new ArrayList<>();
 		for(int i=0;i<inputL.size()-2;i++) {
-			sommeDe3.add(Integer.parseInt(inputL.get(i))+Integer.parseInt(inputL.get(i+1))+Integer.parseInt(inputL.get(i+2)));
+			sommeDe3.add(inputL.get(i)+inputL.get(i+1)+inputL.get(i+2));
 		}
-		for(int i=1;i<sommeDe3.size();i++) {
-			if(sommeDe3.get(i-1)<sommeDe3.get(i)){
-				res++;
-			}
-		}
-		
-		
-		System.out.println("s2 : "+res);
+		System.out.println("s2 : "+getNbIncrease(sommeDe3));
 		
 	}
 	
