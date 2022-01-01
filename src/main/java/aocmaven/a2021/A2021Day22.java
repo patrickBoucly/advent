2635ecc8b1ab26009cb8be6bcdc9ebfc9d3d4258
Patrick Boucly <patrick.boucly@gmail.com>
2022-01-01 20:16:33 +0100

    d25p1

--- src/main/java/aocmaven/a2021/A2021Day22.java
+++ src/main/java/aocmaven/a2021/A2021Day22.java
@@ -1,7 +1,14 @@
 package aocmaven.a2021;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class A2021Day22 extends A2021 {
 
@@ -12,7 +19,7 @@ public class A2021Day22 extends A2021 {
 	public static void main(String[] args0) {
 		A2021Day22 d = new A2021Day22(22);
 		long startTime = System.currentTimeMillis();
-	//	System.out.println(d.s1(true));
+		//System.out.println(d.s1(true));
 		long endTime = System.currentTimeMillis();
 		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
@@ -22,13 +29,361 @@ public class A2021Day22 extends A2021 {
 				+ (endTime - startTime) + " milliseconds");
 	}
 
-	private int s1(boolean b) {
-		
-		return 0;
+	private long s1(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Instruction> instructions = new ArrayList<>();
+		for (String l : lignes) {
+			String[] sp1 = l.split(" ");
+			String[] sp2 = sp1[1].split(",");
+			boolean ins;
+			if (sp1[0].equals("on")) {
+				ins = true;
+			} else {
+				ins = false;
+			}
+			String xmin = "";
+			String xmax = "";
+			String ymin = "";
+			String ymax = "";
+			String zmin = "";
+			String zmax = "";
+			String v = sp2[0];
+			int pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				xmin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				xmax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			v = sp2[1];
+			pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				ymin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				ymax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			v = sp2[2];
+			pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				zmin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				zmax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			instructions.add(new Instruction(ins, Integer.parseInt(xmin), Integer.parseInt(xmax),
+					Integer.parseInt(ymin), Integer.parseInt(ymax), Integer.parseInt(zmin), Integer.parseInt(zmax)));
+		}
+		Set<Point> cube = new HashSet<A2021Day22.Point>();
+		for (int i = -50; i <= 50; i++) {
+			for (int j = -50; j <= 50; j++) {
+				for (int k = -50; k <= 50; k++) {
+					cube.add(new Point(i, j, k, false));
+				}
+			}
+		}
+		int cpt = 0;
+		System.out.println(instructions.size());
+		for (Instruction ins : instructions) {
+			System.out.println(cpt);
+			cube = applyIns1(cube, ins);
+			cpt++;
+		}
+		System.out.println("s1 res : " + cube.stream().filter(Point::isOn).count());
+		return cube.stream().filter(Point::isOn).count();
 	}
 
-	private int s2(boolean b) {
-		return 0;
+	private Set<Point> applyIns1(Set<Point> cube, Instruction ins) {
+		if (!ins.isOn) {
+			cube.removeIf(p -> ins.contient(p));
+		} else {
+			cube.removeIf(p -> ins.contient(p) && !p.isOn);
+			cube.addAll(ins.pointsConcernes1());
+		}
+		return cube;
+	}
+	private Set<Point> applyIns2(Set<Point> cube, Instruction ins) {
+		if (!ins.isOn) {
+			cube.removeIf(p -> ins.contient(p));
+		} else {
+			cube.removeIf(p -> ins.contient(p) && !p.isOn);
+			cube.addAll(ins.pointsConcernes2());
+		}
+		return cube;
+	}
+
+
+	private long s2(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Instruction> instructions = new ArrayList<>();
+		for (String l : lignes) {
+			String[] sp1 = l.split(" ");
+			String[] sp2 = sp1[1].split(",");
+			boolean ins;
+			if (sp1[0].equals("on")) {
+				ins = true;
+			} else {
+				ins = false;
+			}
+			String xmin = "";
+			String xmax = "";
+			String ymin = "";
+			String ymax = "";
+			String zmin = "";
+			String zmax = "";
+			String v = sp2[0];
+			int pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				xmin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				xmax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			v = sp2[1];
+			pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				ymin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				ymax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			v = sp2[2];
+			pos = 2;
+			while (!v.substring(pos, pos + 1).equals(".")) {
+				zmin += v.substring(pos, pos + 1);
+				pos++;
+			}
+			pos += 2;
+			for (int j = pos; j < v.length(); j++) {
+				zmax += v.substring(pos, pos + 1);
+				pos++;
+			}
+			instructions.add(new Instruction(ins, Integer.parseInt(xmin), Integer.parseInt(xmax),
+					Integer.parseInt(ymin), Integer.parseInt(ymax), Integer.parseInt(zmin), Integer.parseInt(zmax)));
+		}
+		Set<Point> cube = new HashSet<A2021Day22.Point>();
+		for (int i = -50; i <= 50; i++) {
+			for (int j = -50; j <= 50; j++) {
+				for (int k = -50; k <= 50; k++) {
+					cube.add(new Point(i, j, k, false));
+				}
+			}
+		}
+		int cpt = 0;
+		System.out.println(instructions.size());
+		for (Instruction ins : instructions) {
+			System.out.println(cpt);
+			cube = applyIns2(cube, ins);
+			cpt++;
+		}
+		System.out.println("s1 res : " + cube.stream().filter(Point::isOn).count());
+		return cube.stream().filter(Point::isOn).count();
+	}
+
+	public static class Point {
+		int x;
+		int y;
+		int z;
+		boolean isOn;
+
+		public Point(int x, int y, int z, boolean isOn) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.isOn = isOn;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(x, y, z);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Point other = (Point) obj;
+			return x == other.x && y == other.y && z == other.z;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public int getZ() {
+			return z;
+		}
+
+		public void setZ(int z) {
+			this.z = z;
+		}
+
+		public boolean isOn() {
+			return isOn;
+		}
+
+		public void setOn(boolean isOn) {
+			this.isOn = isOn;
+		}
+
+		@Override
+		public String toString() {
+			return "Point [x=" + x + ", y=" + y + ", z=" + z + ", isOn=" + isOn + "]";
+		}
+
+	}
+
+	public static class Instruction {
+		boolean isOn;
+		int xmin;
+		int xmax;
+		int ymin;
+		int ymax;
+		int zmin;
+		int zmax;
+
+		public boolean getIsOn() {
+			return isOn;
+		}
+
+		public Set<Point> pointsConcernes1() {
+			Set<Point> cube = new HashSet<>();
+			if (xmin < -50 || xmax > 50 || ymin < -50 || ymax > 50 || zmin < -50 || zmax > 50) {
+
+			} else {
+				for (int i = xmin; i <= xmax; i++) {
+					for (int j = ymin; j <= ymax; j++) {
+						for (int k = zmin; k <= zmax; k++) {
+							cube.add(new Point(i, j, k, true));
+						}
+					}
+				}
+			}
+			return cube;
+		}
+		public Set<Point> pointsConcernes2() {
+			Set<Point> cube = new HashSet<>();
+			
+				for (int i = xmin; i <= xmax; i++) {
+					for (int j = ymin; j <= ymax; j++) {
+						for (int k = zmin; k <= zmax; k++) {
+							cube.add(new Point(i, j, k, true));
+						}
+					}
+				}
+		
+			return cube;
+		}
+
+		public boolean contient(Point p) {
+			if (p.x >= xmin && p.x <= xmax && p.y >= ymin && p.y <= ymax && p.z >= zmin && p.z <= zmax) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public String toString() {
+			return isOn + ", x=" + xmin + ".." + xmax + ", y=" + ymin + ".." + ymax + ", z=" + zmin + ".." + zmax;
+		}
+
+		public void setIsOn(boolean isOn) {
+			this.isOn = isOn;
+		}
+
+		public int getXmin() {
+			return xmin;
+		}
+
+		public void setXmin(int xmin) {
+			this.xmin = xmin;
+		}
+
+		public int getXmax() {
+			return xmax;
+		}
+
+		public void setXmax(int xmax) {
+			this.xmax = xmax;
+		}
+
+		public int getYmin() {
+			return ymin;
+		}
+
+		public void setYmin(int ymin) {
+			this.ymin = ymin;
+		}
+
+		public int getYmax() {
+			return ymax;
+		}
+
+		public void setYmax(int ymax) {
+			this.ymax = ymax;
+		}
+
+		public int getZmin() {
+			return zmin;
+		}
+
+		public void setZmin(int zmin) {
+			this.zmin = zmin;
+		}
+
+		public int getZmax() {
+			return zmax;
+		}
+
+		public void setZmax(int zmax) {
+			this.zmax = zmax;
+		}
+
+		public Instruction(boolean isOn, int xmin, int xmax, int ymin, int ymax, int zmin, int zmax) {
+			super();
+			this.isOn = isOn;
+			this.xmin = xmin;
+			this.xmax = xmax;
+			this.ymin = ymin;
+			this.ymax = ymax;
+			this.zmin = zmin;
+			this.zmax = zmax;
+		}
+
 	}
 
 	public static List<Long> getDuration() {
---
+++ src/main/java/aocmaven/a2021/A2021Day25.java
@@ -0,0 +1,287 @@
+package aocmaven.a2021;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import outils.MesOutils;
+
+public class A2021Day25 extends A2021 {
+
+	public A2021Day25(int day) {
+		super(day);
+	}
+
+	public static void main(String[] args0) {
+		A2021Day25 d = new A2021Day25(25);
+		long startTime = System.currentTimeMillis();
+		System.out.println(d.s1(false));
+		long endTime = System.currentTimeMillis();
+		long timeS1 = endTime - startTime;
+		startTime = System.currentTimeMillis();
+		System.out.println(d.s2(true));
+		endTime = System.currentTimeMillis();
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
+	}
+
+	private int s1(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<SeaC> seacs = new ArrayList<>();
+		int j = 0;
+		for (String l : lignes) {
+			int max = l.length();
+			for (int pos = 0; pos < max; pos++) {
+				if (l.substring(pos, pos + 1).equals(">")) {
+					seacs.add(new SeaC(pos, j, ">"));
+				} else if (l.substring(pos, pos + 1).equals("v")) {
+					seacs.add(new SeaC(pos, j, "v"));
+				} else if (l.substring(pos, pos + 1).equals(".")) {
+					seacs.add(new SeaC(pos, j, "."));
+				}
+			}
+			j++;
+		}
+		Situation s = new Situation(seacs, true, 0);
+		System.out.println(SeaC.listString(seacs));
+		int xmax = MesOutils.getMaxIntegerFromList(seacs.stream().map(SeaC::getX).collect(Collectors.toList()));
+		int ymax = MesOutils.getMaxIntegerFromList(seacs.stream().map(SeaC::getY).collect(Collectors.toList()));
+		while (s.abouge) {
+			s.nextStep(xmax, ymax);
+			System.out.println(s.nbStep);
+		}
+
+		return s.nbStep;
+	}
+
+	private int s2(boolean b) {
+		return 0;
+	}
+
+	public static class Situation {
+
+		public int nbStep;
+
+		public Situation(List<SeaC> seacs, boolean abouge, int i) {
+			super();
+			this.seacs = seacs;
+			this.abouge = abouge;
+			this.nbStep = i;
+		}
+
+		public void nextStep(int xmax, int ymax) {
+			List<SeaC> newSeacs = new ArrayList<>();
+			List<SeaC> canMove = canMove(">", seacs, xmax, ymax);
+			List<SeaC> canTMove = canTMove(">", seacs, xmax, ymax);
+			newSeacs.addAll(canTMove);
+			for (SeaC sc : canMove) {
+				newSeacs.add(sc.move(xmax, ymax));
+			}
+			if (canMove.isEmpty()) {
+				abouge = false;
+			}
+			newSeacs.addAll(seacs.stream().filter(y->y.type.equals("v")).collect(Collectors.toList()));
+			this.seacs = newSeacs;
+			//System.out.println(SeaC.listString(seacs));
+			newSeacs = seacs.stream().filter(y->y.type.equals(">")).collect(Collectors.toList());
+			canMove = canMove("v", seacs, xmax, ymax);
+			canTMove = canTMove("v", seacs, xmax, ymax);
+			newSeacs.addAll(canTMove);
+			for (SeaC sc : canMove) {
+				newSeacs.add(sc.move(xmax, ymax));
+			}
+			System.out.println(SeaC.listString(newSeacs));
+			if (canMove.isEmpty() && !abouge) {
+				abouge = false;
+			}
+			this.seacs = newSeacs;
+			this.nbStep++;
+		}
+
+		private List<SeaC> canTMove(String type, List<SeaC> cc, int xmax, int ymax) {
+			List<SeaC> canTMove = new ArrayList<>();
+			for (SeaC sc : cc) {
+				if (sc.type.equals(type)) {
+					if (sc.type.equals(">")) {
+						if (sc.x == xmax) {
+							if ((cc.contains(new SeaC(0, sc.y, ">")) || cc.contains(new SeaC(0, sc.y, "v")))) {
+								canTMove.add(sc);
+							}
+						} else {
+							if ((cc.contains(new SeaC(sc.x + 1, sc.y, ">"))|| cc.contains(new SeaC(sc.x + 1, sc.y, "v")))) {
+								canTMove.add(sc);
+							}
+						}
+					} else if (sc.type.equals("v")) {
+						if (sc.y == ymax) {
+							if ((cc.contains(new SeaC(sc.x, 0, ">"))||cc.contains(new SeaC(sc.x, 0, "v")))) {
+								canTMove.add(sc);
+							}
+						} else {
+							if ((cc.contains(new SeaC(sc.x, sc.y + 1, ">"))|| cc.contains(new SeaC(sc.x, sc.y + 1, "v")))) {
+								canTMove.add(sc);
+							}
+						}
+					}
+				}
+			}
+			return canTMove;
+		}
+
+		private List<SeaC> canMove(String type, List<SeaC> cc, int xmax, int ymax) {
+			List<SeaC> canMove = new ArrayList<>();
+			for (SeaC sc : cc) {
+				if (sc.type.equals(type)) {
+					if (sc.type.equals(">")) {
+						if (sc.x == xmax) {
+							if (!(cc.contains(new SeaC(0, sc.y, ">")) || cc.contains(new SeaC(0, sc.y, "v")))) {
+								canMove.add(sc);
+							}
+						} else {
+							if (!(cc.contains(new SeaC(sc.x + 1, sc.y, ">"))|| cc.contains(new SeaC(sc.x + 1, sc.y, "v")))) {
+								canMove.add(sc);
+							}
+						}
+					} else if (sc.type.equals("v")) {
+						if (sc.y == ymax) {
+							if (!(cc.contains(new SeaC(sc.x, 0, ">"))||cc.contains(new SeaC(sc.x, 0, "v")))) {
+								canMove.add(sc);
+							}
+						} else {
+							if (!(cc.contains(new SeaC(sc.x, sc.y + 1, ">"))|| cc.contains(new SeaC(sc.x, sc.y + 1, "v")))) {
+								canMove.add(sc);
+							}
+						}
+					}
+				}
+			}
+			return canMove;
+		}
+
+		List<SeaC> seacs;
+		boolean abouge;
+	}
+
+	public static class SeaC {
+		int x;
+		int y;
+
+		@Override
+		public String toString() {
+			return "SeaC [x=" + x + ", y=" + y + ", type=" + type + "]";
+		}
+
+		String type;
+
+		public int getX() {
+			return x;
+		}
+
+		public SeaC move(int xmax, int ymax) {
+			if (type.equals(">")) {
+				if (x == xmax) {
+					return new SeaC(0, y, type);
+				} else {
+					return new SeaC(x + 1, y, type);
+				}
+			} else if (type.equals("v")) {
+				if (y == ymax) {
+					return new SeaC(0, y, type);
+				} else {
+					return new SeaC(x, y + 1, type);
+				}
+			}
+			return null;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public String getType() {
+			return type;
+		}
+
+		public void setType(String type) {
+			this.type = type;
+		}
+
+		public SeaC(int x, int y, String type) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.type = type;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(type, x, y);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			SeaC other = (SeaC) obj;
+			return Objects.equals(type, other.type) && x == other.x && y == other.y;
+		}
+
+		public static String listString(List<SeaC> sc) {
+			StringBuilder res = new StringBuilder();
+			// pts.sort(Comparator.comparing(Point::getY).thenComparing(Comparator.comparing(Point::getX)));
+			int imax = MesOutils.getMaxIntegerFromList(sc.stream().map(SeaC::getX).collect(Collectors.toList()));
+			int imin = MesOutils.getMinIntegerFromList(sc.stream().map(SeaC::getX).collect(Collectors.toList()));
+			int jmax = MesOutils.getMaxIntegerFromList(sc.stream().map(SeaC::getY).collect(Collectors.toList()));
+			int jmin = MesOutils.getMinIntegerFromList(sc.stream().map(SeaC::getY).collect(Collectors.toList()));
+			for (int j = jmin; j <= jmax; j++) {
+				for (int i = imin; i <= imax; i++) {
+					SeaC.getSeaC(sc, i, j).ifPresentOrElse(pt -> res.append(pt.type), () -> res.append("."));
+
+				}
+				res.append("\n");
+			}
+			return res.toString();
+		}
+
+		public static Optional<SeaC> getSeaC(List<SeaC> sc, int x, int y) {
+			SeaC p = null;
+			for (SeaC i : sc) {
+				if (x == i.x && y == i.y) {
+					p = new SeaC(x, y, i.type);
+				}
+			}
+			return Optional.ofNullable(p);
+		}
+
+	}
+
+	public static List<Long> getDuration() {
+		A2021Day25 d = new A2021Day25(25);
+		long startTime = System.currentTimeMillis();
+		d.s1(true);
+		long endTime = System.currentTimeMillis();
+		long timeS1 = endTime - startTime;
+		startTime = System.currentTimeMillis();
+		d.s2(true);
+		endTime = System.currentTimeMillis();
+		return Arrays.asList(timeS1, endTime - startTime);
+	}
+
+}
