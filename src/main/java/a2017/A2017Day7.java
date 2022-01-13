313edfff3e8f821cc626070c206608e8742555b2
Patrick Boucly <patrick.boucly@insee.fr>
2022-01-13 01:58:44 +0100

    2017d7&8

--- src/main/java/a2017/A2017Day7.java
+++ src/main/java/a2017/A2017Day7.java
@@ -1,8 +1,16 @@
 package a2017;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import outils.MesOutils;
 
 public class A2017Day7 extends A2017 {
 
@@ -11,27 +19,366 @@ public class A2017Day7 extends A2017 {
 	}
 
 	public static void main(String[] args0) {
-		A2017Day7 d = new A2017Day7(1);
+		A2017Day7 d = new A2017Day7(7);
 		// d.s1(true);
 		long startTime = System.currentTimeMillis();
-		d.s1(true);
+		// System.out.println(d.s1(true));
 		long endTime = System.currentTimeMillis();
-		long timeS1=endTime - startTime;
+		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		d.s2(true);
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-		System.out.println("Day "+ d.day+" run 1 took "+timeS1+" milliseconds, run 2 took " + (endTime - startTime) + " milliseconds");
-		
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
+
 	}
 
-	public int s1(boolean b) {
-		return 0;
+	public String s1(boolean b) {
+		List<Prog> pgrs = getProgFromInput(b);
+		Set<String> pg = new HashSet<>();
+		Set<String> spg = new HashSet<>();
+		pg = pgrs.stream().map(Prog::getNom).collect(Collectors.toSet());
+		for (Prog p : pgrs) {
+			if (p.subProg != null) {
+				spg.addAll(p.subProg.stream().map(Prog::getNom).collect(Collectors.toList()));
+			}
+		}
+		String nomBase = "";
+		for (String s : pg) {
+			if (!spg.contains(s)) {
+				nomBase = s;
+			}
+		}
+
+		return nomBase;
+	}
+
+	private List<Prog> getProgFromInput(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Prog> pgrs = new ArrayList<>();
+		for (String l : lignes) {
+			if (l.contains(">")) {
+				String[] spi = l.split("->");
+				String[] sp = spi[0].split(" ");
+				String nom = sp[0].trim();
+				int pds = Integer.parseInt(sp[1].replace("(", "").replace(")", ""));
+				String[] spj = spi[1].split(",");
+				List<String> sousPgNAme = new ArrayList<>();
+				for (String s : spj) {
+					sousPgNAme.add(s.trim());
+				}
+				pgrs.add(new Prog(nom, pds, sousPgNAme, false, 10000));
+			} else {
+				String[] sp = l.split(" ");
+				String nom = sp[0].trim();
+				int pds = Integer.parseInt(sp[1].replace("(", "").replace(")", ""));
+				pgrs.add(new Prog(nom, pds, Arrays.asList("none"), true, 1));
+			}
+		}
+		return pgrs;
 	}
 	public int s2(boolean b) {
-		return 0;
+		List<Prog> pgrs = getProgFromInput(b);
+		HashMap<String,Integer> np=new HashMap<>();
+	//	np.put("rqwgj", getPdsTot("rqwgj",pgrs));
+		
+		np.put("nhrla", getPdsTot("nhrla",pgrs));
+		np.put("titychs", getPdsTot("titychs",pgrs));
+		np.put("tgrfyrx", getPdsTot("tgrfyrx",pgrs));
+		np.put("aoxky", getPdsTot("aoxky",pgrs));
+		np.put("iezlg", getPdsTot("iezlg",pgrs));
+		np.put("yraktfn", getPdsTot("yraktfn",pgrs));	
+		np.put("idfyy", getPdsTot("idfyy",pgrs));
+		np.put("nwpmqu", getPdsTot("nwpmqu",pgrs));
+		np.put("owayyaw", getPdsTot("owayyaw",pgrs));
+		
+		// tajeklj, rkczq, ohyhw, zceovs, gudpriq
+		np.put("tajeklj", getPdsTot("tajeklj",pgrs));
+		np.put("rkczq", getPdsTot("rkczq",pgrs));
+		np.put("ohyhw", getPdsTot("ohyhw",pgrs));
+		np.put("zceovs", getPdsTot("zceovs",pgrs));
+		np.put("gudpriq", getPdsTot("gudpriq",pgrs));
+		
+		List<Integer> pds=np.values().stream().collect(Collectors.toList());
+		System.out.println(np);
+		int dif=MesOutils.getMaxIntegerFromList(pds)-MesOutils.getMinIntegerFromList(pds);
+		Prog p=getProg("aobgmc",pgrs);
+		return p.pds-8;
 	}
 
+	private Integer getPdsTot(String n, List<Prog> pgrs) {
+		int pdsTot=0;
+		Prog p=getProg(n,pgrs);
+		if(p.isFinal) {
+			return p.pds;
+		} else {
+			pdsTot+=p.pds;
+			for(String q:p.nomsSubProg) {
+				pdsTot+=getPdsTot(q,pgrs);
+			}
+			return pdsTot;
+		}
+	}
 
+	private Prog getProg(String n, List<Prog> pgrs) {
+		for(Prog p:pgrs) {
+			if(p.nom.equals(n)) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+	public int s2b(boolean b) {
+		List<Prog> pgrs = getProgFromInput(b);
+		for (Prog p : pgrs) {
+			if (p.nom.equals("pxzzw")) {
+				System.out.println(p.nom + " " + p.nomsSubProg);
+			}
+		}
+		int deepNValue = 1;
+		List<Prog> ident = getIdent(pgrs);
+		List<Prog> pasIdent = getPasIdent(pgrs);
+		System.out.println("");
+		while (pasIdent.size() > 2) {
+			for (Prog x : pasIdent) {
+				for (String spXs : x.nomsSubProg) {
+					for (Prog i : ident) {
+						if (i.nom.equals(spXs)) {
+							x.subProg.add(x);
+							x.pdsTot = x.subProg.stream().map(Prog::getPds).reduce(0, (a, c) -> a + c) + x.pds;
+							
+
+						}
+					}
+				}
+			}
+			/*
+			if (pasIdent.size() == 2) {
+				for (Prog x : pasIdent) {
+					for (String spXs : x.nomsSubProg) {
+						for (Prog i : pgrs) {
+							if (i.nom.equals(spXs)) {
+								x.subProg.add(x);
+								if (x.subProg.size() == x.nomsSubProg.size()) {
+									x.pdsTot = x.subProg.stream().map(Prog::getPds).reduce(0, (a, c) -> a + c) + x.pds;
+								}
+
+							}
+						}
+					}
+				}
+			}
+			*/
+			ident = getIdent(pgrs);
+			pasIdent = getPasIdent(pgrs);
+		}
+
+		
+		for (Prog p : pgrs) {
+			for(Prog q:pasIdent) {
+			//	System.out.println(q.nom+ " "+q.nomsSubProg);
+				if(q.nomsSubProg.contains(p.nom)) {
+					System.out.println(p.nom+" "+p.pdsTot);
+				}
+				
+			}
+		}
+		
+		return 0;
+
+	}
+
+	private List<Prog> getPasIdent(List<Prog> pgrs) {
+		return pgrs.stream().filter(p -> p.getSubProg().isEmpty() && !p.isFinal).collect(Collectors.toList());
+	}
+
+	private List<Prog> getIdent(List<Prog> pgrs) {
+		return pgrs.stream().filter(p -> p.getSubProg().size() == p.getNomsSubProg().size() || p.isFinal)
+				.collect(Collectors.toList());
+	}
+
+	private List<Prog> getDeepNSup(int deepNValue, List<Prog> pgrs) {
+		return pgrs.stream().filter(p -> p.deep > deepNValue).collect(Collectors.toList());
+	}
+
+	private List<Prog> getDeepN(int deepNValue, List<Prog> pgrs) {
+		return pgrs.stream().filter(p -> p.deep == deepNValue).collect(Collectors.toList());
+	}
+
+	public Prog getFN(Prog p, List<Prog> l) {
+		if (!l.contains(p)) {
+			return null;
+		} else {
+			for (Prog li : l) {
+				if (li.nom.equals(p.nom)) {
+					return li;
+				}
+			}
+		}
+		return null;
+	}
+
+	private void complete(Prog p, List<Prog> pgrs) {
+		for (Prog sub : p.subProg) {
+			for (Prog pg : pgrs) {
+				if (pg.nom.equals(sub.nom)) {
+					if (!pg.isFinal) {
+						complete(sub, pgrs);
+					} else {
+						sub.isFinal = true;
+						sub.pds = pg.pds;
+						sub.subProg = null;
+					}
+				}
+			}
+
+		}
+
+	}
+
+	public static class Prog {
+		String nom;
+		Integer pds;
+		List<Prog> subProg;
+		boolean isFinal;
+		boolean added = false;
+		int deep;
+		int pdsTot;
+		List<String> nomsSubProg;
+
+		public List<String> getNomsSubProg() {
+			return nomsSubProg;
+		}
+
+		public void setNomsSubProg(List<String> nomsSubProg) {
+			this.nomsSubProg = nomsSubProg;
+		}
+
+		public int getDeep() {
+			return deep;
+		}
+
+		public void setDeep(int deep) {
+			this.deep = deep;
+		}
+
+		public int getPdsTot() {
+			return pdsTot;
+		}
+
+		public void setPdsTot(int pdsTot) {
+			this.pdsTot = pdsTot;
+		}
+
+		public boolean isAdded() {
+			return added;
+		}
+
+		public boolean isComplete() {
+			if (isFinal) {
+				return true;
+			}
+			for (Prog p : subProg) {
+				if (!p.isFinal && p.subProg.size() == 0) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public void setAdded(boolean added) {
+			this.added = added;
+		}
+
+		public Prog(String nom, Integer pds, List<String> nomsSubProg, boolean isFinal, int d) {
+			super();
+			this.nom = nom;
+			this.pds = pds;
+			this.nomsSubProg = nomsSubProg;
+			this.isFinal = isFinal;
+			this.added = false;
+			this.deep = d;
+			this.subProg = new ArrayList<>();
+
+		}
+
+		public boolean incomplet() {
+			if (isFinal) {
+				return false;
+			}
+			for (Prog p : subProg) {
+				if (p.subProg == null && !p.isFinal) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public boolean isFinal() {
+			return isFinal;
+		}
+
+		public void setFinal(boolean isFinal) {
+			this.isFinal = isFinal;
+		}
+
+		public String getNom() {
+			return nom;
+		}
+
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+
+		public Integer getPds() {
+			return pds;
+		}
+
+		public void setPds(Integer pds) {
+			this.pds = pds;
+		}
+
+		public List<Prog> getSubProg() {
+			return subProg;
+		}
+
+		public void setSubProg(List<Prog> subProg) {
+			this.subProg = subProg;
+		}
+
+		public Prog(String nom, Integer pds, List<String> nomsSubProg) {
+			super();
+			this.nom = nom;
+			this.pds = pds;
+			this.subProg = subProg;
+		}
+
+		@Override
+		public String toString() {
+			return "Prog [nom=" + nom + ", pds=" + pds + ", subProg=" + subProg + ", isFinal=" + isFinal + ", added="
+					+ added + ", deep=" + deep + ", pdsTot=" + pdsTot + "]";
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(nom);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Prog other = (Prog) obj;
+			return Objects.equals(nom, other.nom);
+		}
+
+	}
 
 	public static List<Long> getDuration() {
 		A2017Day7 d = new A2017Day7(1);
@@ -42,6 +389,6 @@ public class A2017Day7 extends A2017 {
 		startTime = System.currentTimeMillis();
 		d.s2(true);
 		endTime = System.currentTimeMillis();
-		return Arrays.asList(timeS1,endTime - startTime);
+		return Arrays.asList(timeS1, endTime - startTime);
 	}
 }
--- src/main/java/a2017/A2017Day8.java
+++ src/main/java/a2017/A2017Day8.java
@@ -1,8 +1,14 @@
 package a2017;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import outils.MesOutils;
 
 public class A2017Day8 extends A2017 {
 
@@ -11,27 +17,181 @@ public class A2017Day8 extends A2017 {
 	}
 
 	public static void main(String[] args0) {
-		A2017Day8 d = new A2017Day8(1);
+		A2017Day8 d = new A2017Day8(8);
 		// d.s1(true);
 		long startTime = System.currentTimeMillis();
-		d.s1(true);
+		System.out.println(d.s1(true));
 		long endTime = System.currentTimeMillis();
-		long timeS1=endTime - startTime;
+		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		d.s2(true);
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-		System.out.println("Day "+ d.day+" run 1 took "+timeS1+" milliseconds, run 2 took " + (endTime - startTime) + " milliseconds");
-		
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
+
 	}
 
 	public int s1(boolean b) {
-		return 0;
+		List<Instruction> inst = getInst(b);
+		System.out.println(inst.size());
+		HashMap<String, Integer> reg = new HashMap<>();
+		Set<String> setReg = inst.stream().map(Instruction::getRegistre).collect(Collectors.toSet());
+		for (String s : setReg) {
+			reg.put(s, 0);
+		}
+		for (Instruction i : inst) {
+			reg = apply(reg, i);
+		}
+		System.out.println(reg);
+		int res = MesOutils.getMaxIntegerFromList(reg.values().stream().collect(Collectors.toList()));
+
+		return res;
 	}
+
+	private HashMap<String, Integer> apply(HashMap<String, Integer> reg, Instruction i) {
+		if (condVerif(i.regC, i.opC, i.valCond, reg)) {
+			if (i.op.equals("inc")) {
+				reg.put(i.registre, reg.get(i.registre) + i.val);
+			} else {
+				reg.put(i.registre, reg.get(i.registre) - i.val);
+			}
+		}
+		return reg;
+	}
+
+	private boolean condVerif(String regC, String opC, Integer v, HashMap<String, Integer> reg) {
+		switch (opC) {
+		case "==":
+			return reg.get(regC).equals(v);
+		case ">":
+			return reg.get(regC) > v;
+		case ">=":
+			return reg.get(regC) >= v;
+		case "<":
+			return reg.get(regC) < v;
+		case "<=":
+			return reg.get(regC) <= v;
+		case "!=":
+			return !reg.get(regC).equals(v);
+		default:
+			System.out.println(opC);
+		}
+		return false;
+	}
+
+	private List<Instruction> getInst(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Instruction> inst = new ArrayList<>();
+		for (String l : lignes) {
+			String[] sp = l.split("if");
+			String[] spG = sp[0].trim().split(" ");
+			String[] spD = sp[1].trim().split(" ");
+			String registre = spG[0].trim();
+			String op = spG[1].trim();
+			Integer val = Integer.parseInt(spG[2].trim());
+			String regC = spD[0].trim();
+			String opC = spD[1].trim();
+			Integer valCond = Integer.parseInt(spD[2].trim());
+			inst.add(new Instruction(registre, op, val, regC, opC, valCond));
+		}
+		return inst;
+	}
+
 	public int s2(boolean b) {
-		return 0;
+		List<Instruction> inst = getInst(b);
+		System.out.println(inst.size());
+		HashMap<String, Integer> reg = new HashMap<>();
+		Set<String> setReg = inst.stream().map(Instruction::getRegistre).collect(Collectors.toSet());
+		for (String s : setReg) {
+			reg.put(s, 0);
+		}
+		int res = 0;
+		for (Instruction i : inst) {
+			reg = apply(reg, i);
+			int nm = MesOutils.getMaxIntegerFromList(reg.values().stream().collect(Collectors.toList()));
+			if (nm > res) {
+				res = nm;
+			}
+		}
+
+		return res;
+
 	}
 
+	public static class Instruction {
+		String registre;
+		String op;
+		Integer val;
+		String regC;
+		String opC;
+		Integer valCond;
 
+		public String getRegistre() {
+			return registre;
+		}
+
+		public void setRegistre(String registre) {
+			this.registre = registre;
+		}
+
+		public String getOp() {
+			return op;
+		}
+
+		public void setOp(String op) {
+			this.op = op;
+		}
+
+		public String getRegC() {
+			return regC;
+		}
+
+		public void setRegC(String regC) {
+			this.regC = regC;
+		}
+
+		public String getOpC() {
+			return opC;
+		}
+
+		public void setOpC(String opC) {
+			this.opC = opC;
+		}
+
+		public Integer getValCond() {
+			return valCond;
+		}
+
+		public void setValCond(Integer valCond) {
+			this.valCond = valCond;
+		}
+
+		public Integer getVal() {
+			return val;
+		}
+
+		public void setVal(Integer val) {
+			this.val = val;
+		}
+
+		public Instruction(String registre, String op, Integer val, String regC, String opC, Integer valCond) {
+			super();
+			this.registre = registre;
+			this.op = op;
+			this.val = val;
+			this.regC = regC;
+			this.opC = opC;
+			this.valCond = valCond;
+		}
+
+		@Override
+		public String toString() {
+			return "Instruction [registre=" + registre + ", op=" + op + ", val=" + val + ", regC=" + regC + ", opC="
+					+ opC + ", valCond=" + valCond + "]";
+		}
+
+	}
 
 	public static List<Long> getDuration() {
 		A2017Day8 d = new A2017Day8(1);
@@ -42,6 +202,6 @@ public class A2017Day8 extends A2017 {
 		startTime = System.currentTimeMillis();
 		d.s2(true);
 		endTime = System.currentTimeMillis();
-		return Arrays.asList(timeS1,endTime - startTime);
+		return Arrays.asList(timeS1, endTime - startTime);
 	}
 }
