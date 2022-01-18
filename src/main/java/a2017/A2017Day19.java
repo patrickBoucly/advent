2f73ce9879133cd97ddade01b37577ee8682b120
Patrick Boucly <patrick.boucly@insee.fr>
2022-01-18 01:57:39 +0100

    2017d19 20

--- src/main/java/a2017/A2017Day19.java
+++ src/main/java/a2017/A2017Day19.java
@@ -1,8 +1,10 @@
 package a2017;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class A2017Day19 extends A2017 {
 
@@ -11,27 +13,230 @@ public class A2017Day19 extends A2017 {
 	}
 
 	public static void main(String[] args0) {
-		A2017Day19 d = new A2017Day19(1);
+		A2017Day19 d = new A2017Day19(19);
 		// d.s1(true);
 		long startTime = System.currentTimeMillis();
-		d.s1(true);
+		//System.out.println(d.s1(true));
 		long endTime = System.currentTimeMillis();
-		long timeS1=endTime - startTime;
+		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		d.s2(true);
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-		System.out.println("Day "+ d.day+" run 1 took "+timeS1+" milliseconds, run 2 took " + (endTime - startTime) + " milliseconds");
-		
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
+
 	}
 
-	public int s1(boolean b) {
-		return 0;
+	public String s1(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Case> l = new ArrayList<>();
+		int i = 0;
+		for (String li : lignes) {
+			for (int j = 0; j < li.length(); j++) {
+				String carac = li.substring(j, j + 1);
+				if (!carac.equals(".")) {
+					l.add(new Case(i, j, li.substring(j, j + 1)));
+				}
+			}
+			i++;
+		}
+		Game g = new Game(l);
+		g.sol1();
+		return g.message;
 	}
+
 	public int s2(boolean b) {
-		return 0;
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Case> l = new ArrayList<>();
+		int i = 0;
+		for (String li : lignes) {
+			for (int j = 0; j < li.length(); j++) {
+				String carac = li.substring(j, j + 1);
+				if (!carac.equals(".")) {
+					l.add(new Case(i, j, li.substring(j, j + 1)));
+				}
+			}
+			i++;
+		}
+		Game g = new Game(l);
+		if(b) {
+			g.sol1();
+		}else {
+			g.sol1_s();
+		}
+		
+		return g.step;
+
 	}
 
+	public static class Game {
+		List<Case> l;
+		String message;
+		Case curCase;
+		String dir;
+		int step=1;
 
+		public List<Case> getL() {
+			return l;
+		}
+
+		public void sol1() {
+			while (!curCase.s.equals("L")) {
+				System.out.println(curCase+" "+dir);
+				move();
+				step++;
+			}
+
+		}
+		public void sol1_s() {
+			System.out.println(step);
+			while (!curCase.s.equals("F")) {
+				System.out.println(curCase+" "+dir);
+				move();
+				step++;
+				System.out.println(step);
+			}
+
+		}
+
+		private void move() {
+			Case nextCar = null;
+			if (dir.equals("S")) {
+				nextCar = g(l, curCase.i + 1, curCase.j);
+			} else if (dir.equals("N")) {
+				nextCar = g(l, curCase.i - 1, curCase.j);
+			} else if (dir.equals("E")) {
+				nextCar = g(l, curCase.i, curCase.j + 1);
+			} else if (dir.equals("W")) {
+				nextCar = g(l, curCase.i, curCase.j - 1);
+			}
+			if (Character.isLetter(nextCar.s.charAt(0))) {
+				message += nextCar.s;
+			}
+			if (nextCar.s.equals("+")) {
+				
+				if (dir.equals("S") || dir.equals("N")) {
+					if (gc(l, nextCar.i, curCase.j - 1)) {
+						Case nextCarg = g(l, nextCar.i, nextCar.j - 1);
+						if (nextCarg.s.equals("-")||Character.isLetter(nextCarg.s.charAt(0))) {
+							dir = "W";
+						}
+					} else {
+						dir = "E";
+					}
+
+				} else if (dir.equals("E") || dir.equals("W")) {
+					if (gc(l, nextCar.i + 1, nextCar.j)) {
+						Case nextCarg = g(l, nextCar.i + 1, nextCar.j);
+						if (nextCarg.s.equals("|")||Character.isLetter(nextCarg.s.charAt(0))) {
+							dir = "S";
+						}
+					} else {
+						dir = "N";
+					}
+				}
+			}
+			curCase = nextCar;
+
+		}
+
+		private boolean gc(List<Case> l, int i, int j) {
+			if (g(l, i, j) == null) {
+				return false;
+			}
+			return true;
+		}
+
+		private Case g(List<Case> l2, int i, int j) {
+			Optional<Case> nc = l.stream().filter(c -> c.i == i && c.j == j).findAny();
+			if (nc.isPresent()) {
+				return nc.get();
+			}
+			return null;
+		}
+
+		public void setL(List<Case> l) {
+			this.l = l;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		public String getDir() {
+			return dir;
+		}
+
+		public void setDir(String dir) {
+			this.dir = dir;
+		}
+
+		public Game(List<Case> l) {
+			super();
+			this.l = l;
+			this.message = "";
+			this.dir = "S";
+			this.curCase = l.stream().filter(c -> c.i == 0).findFirst().get();
+		}
+
+		public Case getCurCase() {
+			return curCase;
+		}
+
+		public void setCurCase(Case curCase) {
+			this.curCase = curCase;
+		}
+
+	}
+
+	public static class Case {
+		int i;
+		int j;
+		String s;
+
+		public int getI() {
+			return i;
+		}
+
+		public void setI(int i) {
+			this.i = i;
+		}
+
+		public int getJ() {
+			return j;
+		}
+
+		public void setJ(int j) {
+			this.j = j;
+		}
+
+		public String getS() {
+			return s;
+		}
+
+		public void setS(String s) {
+			this.s = s;
+		}
+
+		public Case(int i, int j, String s) {
+			super();
+			this.i = i;
+			this.j = j;
+			this.s = s;
+		}
+
+		@Override
+		public String toString() {
+			return "Case [i=" + i + ", j=" + j + ", s=" + s + "]";
+		}
+
+	}
 
 	public static List<Long> getDuration() {
 		A2017Day19 d = new A2017Day19(1);
@@ -42,6 +247,6 @@ public class A2017Day19 extends A2017 {
 		startTime = System.currentTimeMillis();
 		d.s2(true);
 		endTime = System.currentTimeMillis();
-		return Arrays.asList(timeS1,endTime - startTime);
+		return Arrays.asList(timeS1, endTime - startTime);
 	}
 }
--- src/main/java/a2017/A2017Day20.java
+++ src/main/java/a2017/A2017Day20.java
@@ -1,8 +1,13 @@
 package a2017;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import outils.MesOutils;
 
 public class A2017Day20 extends A2017 {
 
@@ -11,27 +16,269 @@ public class A2017Day20 extends A2017 {
 	}
 
 	public static void main(String[] args0) {
-		A2017Day20 d = new A2017Day20(1);
+		A2017Day20 d = new A2017Day20(20);
 		// d.s1(true);
 		long startTime = System.currentTimeMillis();
-		d.s1(true);
+		//System.out.println(d.s1(true));
 		long endTime = System.currentTimeMillis();
-		long timeS1=endTime - startTime;
+		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		d.s2(true);
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-		System.out.println("Day "+ d.day+" run 1 took "+timeS1+" milliseconds, run 2 took " + (endTime - startTime) + " milliseconds");
-		
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
+
 	}
 
-	public int s1(boolean b) {
-		return 0;
-	}
-	public int s2(boolean b) {
-		return 0;
+	public long s1(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Particule> ps = new ArrayList<>();
+		int id = 0;
+		for (String l : lignes) {
+			String[] sp = l.split(",");
+			ps.add(new Particule(id, Long.parseLong(sp[0]), Long.parseLong(sp[1]), Long.parseLong(sp[2]),
+					Long.parseLong(sp[3]), Long.parseLong(sp[4]), Long.parseLong(sp[5]), Long.parseLong(sp[6]),
+					Long.parseLong(sp[7]), Long.parseLong(sp[8])));
+			id++;
+		}
+		for (long j = 0; j < 1000000; j++) {
+			for (Particule p : ps) {
+				p.move();
+			}
+		}
+		long min = 10000000000000L;
+		Particule pmin = null;
+		for (Particule p : ps) {
+			if (p.dist() < min) {
+				pmin = p;
+				min = p.dist();
+			}
+		}
+
+		return pmin.id;
 	}
 
+	public long s2(boolean b) {
+		List<String> lignes = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim)
+				.collect(Collectors.toList());
+		List<Particule> ps = new ArrayList<>();
+		int id = 0;
+		for (String l : lignes) {
+			String[] sp = l.split(",");
+			ps.add(new Particule(id, Long.parseLong(sp[0]), Long.parseLong(sp[1]), Long.parseLong(sp[2]),
+					Long.parseLong(sp[3]), Long.parseLong(sp[4]), Long.parseLong(sp[5]), Long.parseLong(sp[6]),
+					Long.parseLong(sp[7]), Long.parseLong(sp[8])));
+			id++;
+		}
+		for (long j = 0; j < 1000000; j++) {
+			for (Particule p : ps) {
+				p.move();
+			}
+			ps = collisions(ps);
+			System.out.println(ps.size());
+		}
+		return ps.size();
+	}
 
+	private List<Particule> collisions(List<Particule> ps) {
+		HashMap<Position, List<Integer>> poz = new HashMap<>();
+		for (Particule p : ps) {
+			if (poz.containsKey(p.gp())) {
+				List<Integer> l=new ArrayList<>(poz.get(p.gp()));
+				l.add(p.id);
+				poz.put(p.gp(),l);
+			} else {
+				poz.put(p.gp(),Arrays.asList(p.id));
+			}
+		}
+		List<Particule> res=new ArrayList<>(ps);
+		for(Position pz:poz.keySet()) {
+			if(poz.get(pz).size()>1) {
+				for(Integer id:poz.get(pz)) {
+					res=res.stream().filter(p->p.id != id).collect(Collectors.toList());
+				}
+			}
+		}
+		return res;
+	}
+
+	public static class Position {
+		long x;
+		long y;
+		long z;
+
+		public long getX() {
+			return x;
+		}
+
+		public void setX(long x) {
+			this.x = x;
+		}
+
+		public long getY() {
+			return y;
+		}
+
+		public void setY(long y) {
+			this.y = y;
+		}
+
+		public long getZ() {
+			return z;
+		}
+
+		public void setZ(long z) {
+			this.z = z;
+		}
+
+		public Position(long x, long y, long z) {
+			super();
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(x, y, z);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Position other = (Position) obj;
+			return x == other.x && y == other.y && z == other.z;
+		}
+
+	}
+
+	public static class Particule {
+		int id;
+		long x;
+		long y;
+		long z;
+		long vx;
+		long vy;
+		long vz;
+		long ax;
+		long ay;
+		long az;
+
+		public Position gp() {
+			return new Position(x, y, z);
+		}
+
+		public long getX() {
+			return x;
+		}
+
+		public void setX(long x) {
+			this.x = x;
+		}
+
+		public long getY() {
+			return y;
+		}
+
+		public void setY(long y) {
+			this.y = y;
+		}
+
+		public long getZ() {
+			return z;
+		}
+
+		public void setZ(long z) {
+			this.z = z;
+		}
+
+		public long getVx() {
+			return vx;
+		}
+
+		public void setVx(long vx) {
+			this.vx = vx;
+		}
+
+		public long getVy() {
+			return vy;
+		}
+
+		public void setVy(long vy) {
+			this.vy = vy;
+		}
+
+		public long getVz() {
+			return vz;
+		}
+
+		public void setVz(long vz) {
+			this.vz = vz;
+		}
+
+		public long getAx() {
+			return ax;
+		}
+
+		public void setAx(long ax) {
+			this.ax = ax;
+		}
+
+		public long getAy() {
+			return ay;
+		}
+
+		public void setAy(long ay) {
+			this.ay = ay;
+		}
+
+		public long getAz() {
+			return az;
+		}
+
+		public void setAz(long az) {
+			this.az = az;
+		}
+
+		public Particule(int id, long x, long y, long z, long vx, long vy, long vz, long ax, long ay, long az) {
+			super();
+			this.id = id;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.vx = vx;
+			this.vy = vy;
+			this.vz = vz;
+			this.ax = ax;
+			this.ay = ay;
+			this.az = az;
+		}
+
+		@Override
+		public String toString() {
+			return "Particule [x=" + x + ", y=" + y + ", z=" + z + ", vx=" + vx + ", vy=" + vy + ", vz=" + vz + ", ax="
+					+ ax + ", ay=" + ay + ", az=" + az + "]";
+		}
+
+		public void move() {
+			vx += ax;
+			vy += ay;
+			vz += az;
+			x += vx;
+			y += vy;
+			z += vz;
+		}
+
+		public long dist() {
+			return Math.abs(x) + Math.abs(y) + Math.abs(z);
+		}
+	}
 
 	public static List<Long> getDuration() {
 		A2017Day20 d = new A2017Day20(1);
@@ -42,6 +289,6 @@ public class A2017Day20 extends A2017 {
 		startTime = System.currentTimeMillis();
 		d.s2(true);
 		endTime = System.currentTimeMillis();
-		return Arrays.asList(timeS1,endTime - startTime);
+		return Arrays.asList(timeS1, endTime - startTime);
 	}
 }
