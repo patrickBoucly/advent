9291accf464205641d297b61ebebb4f84520e71a
Patrick Boucly <patrick.boucly@gmail.com>
2023-12-09 09:51:42 +0100

    2023d9

--- src/main/java/a2023/A2023Day09.java
+++ src/main/java/a2023/A2023Day09.java
@@ -1,8 +1,19 @@
 package a2023;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import a2023.A2023Day08.Node;
+import a2023.A2023Day08.TheGame;
+import a2023.A2023Day09.Sequence;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import outils.UniformCostSearch.Graph;
 
 public class A2023Day09 extends A2023 {
 
@@ -12,9 +23,9 @@ public class A2023Day09 extends A2023 {
 
 	public static void main(String[] args0) {
 		A2023Day09 d = new A2023Day09(9);
-		System.out.println(d.s1(true));
+		// System.out.println(d.s1(true));
 		long startTime = System.currentTimeMillis();
-		d.s1(true);
+		// d.s1(true);
 		long endTime = System.currentTimeMillis();
 		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
@@ -25,16 +36,132 @@ public class A2023Day09 extends A2023 {
 
 	}
 
-	public int s1(boolean b) {
+	public Long s1(boolean b) {
 		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().collect(Collectors.toList());
-		System.out.println(inputL);
-
-		return 0;
+		TheGame tg = getTheGame(inputL);
+		System.out.println(tg);
+		tg.solve1();
+		Long res=0L;
+		for(Sequence seq:tg.seqs) {
+			res+= seq.seq.get(0).get(seq.seq.get(0).size()-1);
+		}
+		return res;
+		
 	}
 
-	public int s2(boolean b) {
-		return 0;
+	public Long s2(boolean b) {
+		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().collect(Collectors.toList());
+		TheGame tg = getTheGame(inputL);
+		System.out.println(tg);
+		tg.solve2();
+		Long res=0L;
+		for(Sequence seq:tg.seqs) {
+			res+= seq.seq.get(0).get(0);
+		}
+		return res;
 
+	}
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@ToString
+	@NoArgsConstructor
+	public static class TheGame {
+		public void solve1() {
+			for (Sequence sequence : seqs) {
+				goDeep(sequence);
+				goUp(sequence);
+			}
+		}
+
+		public void solve2() {
+			for (Sequence sequence : seqs) {
+				goDeep(sequence);
+				goUp2(sequence);
+			}
+			
+		}
+
+		private void goUp2(Sequence sequence) {
+			for (int j = sequence.seq.size() - 1; j >0; j--) {
+				Long firstJ = sequence.seq.get(j).get(0);
+				Long firstBeforeJ = sequence.seq.get(j-1).get(0);
+				sequence.seq.get(j-1).add(0,firstBeforeJ-firstJ);
+			}
+			
+		}
+
+		private void goUp(Sequence sequence) {
+			for (int j = sequence.seq.size() - 1; j >0; j--) {
+				Long lastJ = sequence.seq.get(j).get(sequence.seq.get(j).size()-1);
+				Long lastBeforeJ = sequence.seq.get(j-1).get(sequence.seq.get(j-1).size()-1);
+				sequence.seq.get(j-1).add(lastJ+lastBeforeJ);
+			}
+		}
+
+		private void goDeep(Sequence sequence) {
+			List<Long> lastList = sequence.getLastList();
+			if (lastList.stream().allMatch(i -> i == 0L)) {
+				lastList.add(0L);
+			} else {
+				sequence.ajouterSub();
+				goDeep(sequence);
+			}
+		}
+
+		List<Sequence> seqs;
+	}
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@ToString
+	@NoArgsConstructor
+	public static class Sequence {
+
+		public List<Long> getLastList() {
+			int m = seq.size();
+			return seq.get(m - 1);
+		}
+
+		public List<Long> getBeforeLastList() {
+			int m = seq.size();
+			return seq.get(m - 2);
+		}
+
+		public void ajouterSub() {
+			List<Long> lastSeq = getLastList();
+			List<Long> valueSeqSubseq = new ArrayList<Long>();
+			for (int i = 0; i < lastSeq.size() - 1; i++) {
+				valueSeqSubseq.add(lastSeq.get(i + 1) - lastSeq.get(i));
+			}
+			List<List<Long>> ll = new ArrayList<List<Long>>();
+			ll.addAll(seq);
+			ll.add(valueSeqSubseq);
+			seq = new ArrayList<List<Long>>();
+			seq.addAll(ll);
+		}
+
+		List<List<Long>> seq;
+
+	}
+
+	private TheGame getTheGame(List<String> inputL) {
+		TheGame tg = new TheGame();
+		List<Sequence> seqs = new ArrayList<>();
+		for (String s : inputL) {
+			List<Long> seq = new ArrayList<Long>();
+			String[] sp = s.split(" ");
+			for (int i = 0; i < sp.length; i++) {
+				seq.add(Long.valueOf(sp[i].trim()));
+			}
+			Sequence sequ = new Sequence();
+			sequ.setSeq(Arrays.asList(seq));
+			seqs.add(sequ);
+		}
+		tg.setSeqs(seqs);
+		return tg;
 	}
 
 	public static List<Long> getDuration() {
