d82a0717f5323772e0215a9cd66c41c5f84d0ccd
Patrick Boucly <patrick.boucly@insee.fr>
2025-12-05 11:02:04 +0100

    a2025d4&5

--- src/main/java/a2025/A2025Day04.java
+++ src/main/java/a2025/A2025Day04.java
@@ -1,59 +1,178 @@
 package a2025;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-
-import org.apache.commons.lang3.tuple.Pair;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import outils.MesOutils;
 
 public class A2025Day04 extends A2025 {
 	public A2025Day04(int day) {
 		super(day);
 	}
+
 	public static void main(String[] args0) {
-		A2025Day04 d = new A2025Day04(3);
-		System.out.println(d.s1(false));
+		A2025Day04 d = new A2025Day04(4);
 		//System.out.println(d.s1(true));
+		// System.out.println(d.s1(true));
 		long startTime = System.currentTimeMillis();
-		//d.s1(true);
+		// d.s1(true);
 		long endTime = System.currentTimeMillis();
 		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		//System.out.println(d.s2(true));
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-	//	System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
-		//		+ (endTime - startTime) + " milliseconds");
+		 System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+		 + (endTime - startTime) + " milliseconds");
 	}
 
-	public Integer s1(boolean b) {
+	public long s1(boolean b) {
 		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim).toList();
-		Game g = new Game(inputL);
+		Game g = new Game(getPointsFromInput(inputL));
 		return g.solve1();
 	}
 
-	public Long s2(boolean b) {
+	public long s2(boolean b) {
 		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim).toList();
-		Game g = new Game(inputL);
+
+		Game g = new Game(getPointsFromInput(inputL));
 		return g.solve2();
 	}
-	@Data
+
+	private Set<Point> getPointsFromInput(List<String> inputL) {
+		Set<Point> points = new HashSet<>();
+		int j = 0;
+		for (String l : inputL) {
+			points.addAll(getPointsFromLigne(l, j));
+			j++;
+		}
+		return points;
+	}
+
+	private Set<Point> getPointsFromLigne(String l, int j) {
+		Set<Point> points = new HashSet<>();
+		for (int i = 0; i < l.length(); i++) {
+			if (l.substring(i, i + 1).equals("@")) {
+				points.add(new Point(i, j, l.substring(i, i + 1),false));
+			}
+		}
+		return points;
+	}
+
+	@Getter
+	@Setter
 	@AllArgsConstructor
+	@ToString
+	@NoArgsConstructor
 	public static class Game {
-		List<String> inputL;
-		public Integer solve1() {
-			int r = 0;			
+		Set<Point> points;
+
+		public Long solve1() {
+			Long r=0L;
+			for(Point p:points) {
+				if(getVoisinnage(p).size()<4) {
+					r++;
+				}
+			}
 			return r;
 		}
-		public Long solve2() {
-			Long r = 0L;
-			return r;
+
+		public long solve2() {
+			boolean cont=true;
+			while(cont) {
+				cont=false;
+				for(Point p:points.stream().filter(t->!t.supprimable).toList()) {
+					if(getVoisinnageSup(p).size()<4) {
+						p.supprimable=true;
+						cont=true;
+					}
+				}
+				
+			}
+			
+			return points.stream().filter(t->t.supprimable).count();
 		}
+		private Set<Point> getVoisinnageSup(Point c) {
+			Set<Point> voisinage=new HashSet<>();
+			Optional<Point> gauche=getPoint(points,c.x-1,c.y);
+			Optional<Point> droite=getPoint(points,c.x+1,c.y);
+			if(gauche.isPresent() && !gauche.get().supprimable ) {
+				voisinage.add(gauche.get());
+			}
+			if(droite.isPresent()&& !droite.get().supprimable) {
+				voisinage.add(droite.get());
+			}
+			for(int k=c.x-1;k<=c.x+1;k++) {
+				Optional<Point> dessus=getPoint(points,k,c.y-1);
+				Optional<Point> dessous=getPoint(points,k,c.y+1);
+				if(dessus.isPresent() && !dessus.get().supprimable) {
+					voisinage.add(dessus.get());
+				}
+				if(dessous.isPresent() &&  !dessous.get().supprimable ) {
+					voisinage.add(dessous.get());
+				}
+			}
+			return voisinage;
+		}
+
+		private Set<Point> getVoisinnage(Point c) {
+			Set<Point> voisinage=new HashSet<>();
+			Optional<Point> gauche=getPoint(points,c.x-1,c.y);
+			Optional<Point> droite=getPoint(points,c.x+1,c.y);
+			if(gauche.isPresent()) {
+				voisinage.add(gauche.get());
+			}
+			if(droite.isPresent()) {
+				voisinage.add(droite.get());
+			}
+			for(int k=c.x-1;k<=c.x+1;k++) {
+				Optional<Point> dessus=getPoint(points,k,c.y-1);
+				Optional<Point> dessous=getPoint(points,k,c.y+1);
+				if(dessus.isPresent()) {
+					voisinage.add(dessus.get());
+				}
+				if(dessous.isPresent()) {
+					voisinage.add(dessous.get());
+				}
+			}
+			return voisinage;
+		}
+
+	}
+	
+	
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@ToString
+	@NoArgsConstructor
+	private static class Point {
+		int x;
+		int y;
+		String contenu;
+		boolean supprimable;
+	}
+
+	public static Optional<Point> getPoint(Set<Point> pts, int x, int y) {
+		Point p = null;
+		for (Point i : pts) {
+			if (x == i.x && y == i.y) {
+				p = new Point(x, y, i.contenu,i.supprimable);
+			}
+		}
+		return Optional.ofNullable(p);
 	}
 
 	public static List<Long> getDuration() {
-		A2025Day04 d = new A2025Day04(3);
+		A2025Day04 d = new A2025Day04(4);
 		long startTime = System.currentTimeMillis();
 		d.s1(true);
 		long endTime = System.currentTimeMillis();
--- src/main/java/a2025/A2025Day05.java
+++ src/main/java/a2025/A2025Day05.java
@@ -1,59 +1,113 @@
 package a2025;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-
-import org.apache.commons.lang3.tuple.Pair;
+import java.util.Set;
+import java.util.TreeSet;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class A2025Day05 extends A2025 {
 	public A2025Day05(int day) {
 		super(day);
 	}
 	public static void main(String[] args0) {
-		A2025Day05 d = new A2025Day05(3);
-		System.out.println(d.s1(false));
+		A2025Day05 d = new A2025Day05(5);
+		//System.out.println(d.s1(true));
 		//System.out.println(d.s1(true));
 		long startTime = System.currentTimeMillis();
-		//d.s1(true);
+		d.s1(true);
 		long endTime = System.currentTimeMillis();
 		long timeS1 = endTime - startTime;
 		startTime = System.currentTimeMillis();
-		//System.out.println(d.s2(true));
+		System.out.println(d.s2(true));
 		endTime = System.currentTimeMillis();
-	//	System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
-		//		+ (endTime - startTime) + " milliseconds");
+		System.out.println("Day " + d.day + " run 1 took " + timeS1 + " milliseconds, run 2 took "
+				+ (endTime - startTime) + " milliseconds");
 	}
 
-	public Integer s1(boolean b) {
+	public Long s1(boolean b) {
 		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim).toList();
-		Game g = new Game(inputL);
+		Game g = new Game();
+		g.setValues(inputL);
 		return g.solve1();
 	}
 
 	public Long s2(boolean b) {
 		List<String> inputL = Arrays.asList(getInput(b).split("\n")).stream().map(String::trim).toList();
-		Game g = new Game(inputL);
+		Game g = new Game();
+		g.setValues(inputL);
 		return g.solve2();
 	}
 	@Data
-	@AllArgsConstructor
+	@NoArgsConstructor
 	public static class Game {
-		List<String> inputL;
-		public Integer solve1() {
-			int r = 0;			
-			return r;
+		List<Range> range;
+		List<Long> produit;
+		public Long solve1() {
+			return produit.stream().filter(l->range.stream().anyMatch(r->r.between(l))).count();
+		}
+		public void setValues(List<String> inputL) {
+			List<Range> range = new ArrayList<>();
+			List<Long> produit = new ArrayList<>();
+			boolean p=false;
+			for(int i=0;i<inputL.size();i++) {
+				if(inputL.get(i).isEmpty()) {
+					p=true;
+				}
+				else if(p) {
+					produit.add(Long.parseLong(inputL.get(i)));
+				}else {
+					String[] s=inputL.get(i).split("-");
+					range.add(new Range(Long.parseLong(s[0]),Long.parseLong(s[1])));
+				}
+			}
+			this.range=range;
+			this.produit=produit;
 		}
 		public Long solve2() {
-			Long r = 0L;
-			return r;
+			
+			Set<Long> border=new HashSet<>();
+			
+			for(Range r:range) {
+				border.add(r.max);
+				border.add(r.min);
+			}	
+			Long freshCpt=0L+border.size();
+			List<Long> orderList= new ArrayList<>(new TreeSet<>(border));
+
+			for(int i=0;i<orderList.size()-1;i++) {
+				if(orderList.get(i+1)-orderList.get(i)>1) {
+					if(iIsOk(orderList.get(i)+1)) {
+						
+						freshCpt+=orderList.get(i+1)-orderList.get(i)-1;
+					}
+				}
+				
+			}
+			return freshCpt;	
+		}
+		private boolean iIsOk(long l) {
+			return range.stream().anyMatch(r->r.between(l));
+		}
+	}
+	
+	@Data
+	@AllArgsConstructor
+	public static class Range {
+		Long min;
+		Long max;
+		public Boolean between(Long l) {
+			return min<=l && l<=max;
 		}
 	}
 
 	public static List<Long> getDuration() {
-		A2025Day05 d = new A2025Day05(3);
+		A2025Day05 d = new A2025Day05(5);
 		long startTime = System.currentTimeMillis();
 		d.s1(true);
 		long endTime = System.currentTimeMillis();
